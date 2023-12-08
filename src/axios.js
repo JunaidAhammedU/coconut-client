5cb0b9970ff7da91db5b0bde022f8a6414a181ea
JunaidAhammedU <junaidahammed15@gmail.com>
2023-12-08 21:55:54 +0530

    a

--- src/axios.js
+++ src/axios.js
@@ -1,16 +1,38 @@
+// import axios from "axios";
+
+// const api_request = axios.create({
+//     baseURL: "https://oleaplants.shop"
+// })
+
+// api_request.interceptors.request.use(config =>{
+//     const path = config.url.split('/')[1];
+//     const userAuth = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")).token : null;
+//     const adminAuth = localStorage.getItem("admin") ? JSON.parse(localStorage.getItem("admin")).token : null;
+//     const authToken = path === "admin" ? adminAuth : userAuth;
+//     config.headers.Authorization = `Bearer ${authToken}`;
+//     return config;
+// })
+
+// export default api_request;
+
+// axios setup
 import axios from "axios";
 
 const api_request = axios.create({
-    baseURL: "https://oleaplants.shop"
-})
+  baseURL: "https://oleaplants.shop",
+});
 
-api_request.interceptors.request.use(config =>{
-    const path = config.url.split('/')[1];
-    const userAuth = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")).token : null;
-    const adminAuth = localStorage.getItem("admin") ? JSON.parse(localStorage.getItem("admin")).token : null;
-    const authToken = path === "admin" ? adminAuth : userAuth;
-    config.headers.Authorization = `Bearer ${authToken}`;
-    return config;
-})
+api_request.interceptors.request.use((config) => {
+  const path = config.url.split("/")[1];
+  const userAuth = localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user")).token
+    : null;
+  const adminAuth = localStorage.getItem("admin")
+    ? JSON.parse(localStorage.getItem("admin")).token
+    : null;
+  const authToken = path === "admin" ? adminAuth : userAuth;
+  config.headers.Authorization = `Bearer ${authToken}`;
+  return config;
+});
 
 export default api_request;
