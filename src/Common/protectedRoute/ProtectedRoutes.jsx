15fa1dfbb29b6dd937d0407ef75ea044523e96e5
JunaidAhammedU <junaidahammed15@gmail.com>
2023-12-21 16:07:45 +0530

    update

--- src/Common/protectedRoute/ProtectedRoutes.jsx
+++ src/Common/protectedRoute/ProtectedRoutes.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 //------------------------------------------
 
 const ProtectedRoutes = ({ element }) => {
-  const { user } = useSelector((state) => state);
+  const { user } = useSelector((state) => state.user);
   if (!user) {
     return <Navigate to={"/login"} replace />;
   }
